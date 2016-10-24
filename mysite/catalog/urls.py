a26854fa744b399cb10afffe78bc3296d2423f6e
TropicalCode <tropicalbooranasit@gmail.com>
2016-10-25 01:53:11 +0700

    New catalog

--- mysite/catalog/urls.py
+++ mysite/catalog/urls.py
@@ -3,14 +3,20 @@ from django.contrib import admin
 
 from .views import (
 	allcatalog,
+	skincare,
+	base_makeup,
+	point_makeup,
+	accessories,
+	fragrance,
 	)
 
 urlpatterns = [
 	url(r'^$', allcatalog),
-    #url(r'^create/$', post_create),
-    #url(r'^detail/$', post_detail),
-    #url(r'^update/$', post_update),
-    #url(r'^delete/$', post_delete),
+    url(r'^skincare/$', skincare),
+    url(r'^base_makeup/$', base_makeup),
+    url(r'^point_makeup/$', point_makeup),
+    url(r'^accessories/$', accessories),
+    url(r'^fragrance/$', fragrance),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -10,3 +10,43 @@ def allcatalog(request):
 		"title": "List",
 	}
 	return render(request, "catalog.html", context)
+
+def skincare(request):
+	queryset = Product.objects.all()
+	context = {
+		"object_list": queryset, 
+		"title": "List",
+	}
+	return render(request, "Skincare.html", context)
+
+def base_makeup(request):
+	queryset = Product.objects.all()
+	context = {
+		"object_list": queryset, 
+		"title": "List",
+	}
+	return render(request, "base_makeup.html", context)
+
+def point_makeup(request):
+	queryset = Product.objects.all()
+	context = {
+		"object_list": queryset, 
+		"title": "List",
+	}
+	return render(request, "point_makeup.html", context)
+
+def accessories(request):
+	queryset = Product.objects.all()
+	context = {
+		"object_list": queryset, 
+		"title": "List",
+	}
+	return render(request, "accessories.html", context)
+
+def fragrance(request):
+	queryset = Product.objects.all()
+	context = {
+		"object_list": queryset, 
+		"title": "List",
+	}
+	return render(request, "fragrance.html", context)
