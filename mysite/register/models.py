45b728871e4e58fb0609af09dd89e4655d8f6224
napnap11 <khochapak@gmail.com>
2016-11-15 22:00:22 +0700

    Update models.py

--- mysite/register/models.py
+++ mysite/register/models.py
@@ -8,3 +8,6 @@ class UserData(models.Model):
 	gender = models.CharField(max_length=45)
 	dateofbirth = models.CharField(max_length=45)
 	phonenum = models.CharField(max_length=45)
+	class Meta:
+		db_table = "userdata"		
+ 		app_label = 'Users' 
