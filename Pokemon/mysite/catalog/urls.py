47b560e4e44ffcb900ccde1b4ba5c070c71b02f7
TropicalCode <tropicalbooranasit@gmail.com>
2016-11-18 20:44:52 +0700

    addproduct with link but plain form

--- Pokemon/mysite/catalog/urls.py
+++ Pokemon/mysite/catalog/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
 from django.contrib import admin
-
-from .views import (
-	allcatalog,
-	)
+from .import views
 
 urlpatterns = [
-	url(r'^$', allcatalog),
-    #url(r'^posts/$', "<appname>.views.<function_name>"),
+	url(r'^$', views.allcatalog),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),  
+    url(r'^addproduct/$', views.index, name='Index'),
+	url(r'^showdata/$', views.showdata),
+	url(r'^select/$', views.select),
+	url(r'^update/$', views.update),
+	url(r'^doUpdate/$', views.doUpdate),
+	url(r'^delete/$', views.delete),
 ]
 
 
