e1bbcaef9e263f1ec1338bce40887a00d859b73f
Linspin <blackspinxz@gmail.com>
2016-11-21 14:11:15 +0700

    edit Profile

--- mysite/profiles/urls.py
+++ mysite/profiles/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index),
+	url(r'^editprofile/$', views.editpro),
+	url(r'^editpassword/$', views.editpass),
+	url(r'^updatepro/$', views.updatepro),
+	url(r'^updatepass/$', views.updatepass),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
 
