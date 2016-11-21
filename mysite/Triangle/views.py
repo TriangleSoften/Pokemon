91378a4dadbc4914e8cdbad0a17b1fac41229154
napnap11 <khochapak@gmail.com>
2016-11-21 12:24:31 +0700

    Add files via upload

--- mysite/Triangle/views.py
+++ mysite/Triangle/views.py
@@ -3,36 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
-def post_create(request):
-	return HttpResponse("<h1>Create</h1>")
-
-def post_detail(request): #retrieve
-	context = {
-		"title": "Detail"
-	}
-	return render(request, "index.html", context)
-
 def post_list(request): #list items
-
-	#context = {
-	#	"title": "List"
-	#}
-	
-	# if request.user.is_authenticated():
-	# 	context = {
-	# 		"title": "My User List"
-	# 	}
-	# else:
-	# 	context = {
-	# 		"title": "List"
-	# 	}
-	return render(request, "codeboost.html")
-
-
-def post_update(request):
-	return HttpResponse("<h1>Update</h1>")
-
-
-def post_delete(request):
-	return HttpResponse("<h1>Delete</h1>")
+	userin=request.user.username
+	print("hello")
+	print(userin)
+	if (userin == ""):
+		logged = 0
+	else :
+		logged = 1
+	context = {
+		"title": "List",
+		"user": userin,
+		"logged": logged,
+	}
+	return render(request, "codeboost.html", context)
