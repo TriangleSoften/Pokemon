b8622174cb4e5316a65b198f2214225011e94f67
NLito = <NLito =>
2016-11-28 04:52:10 +0700

    sleep now

--- mysite/catalog/urls.py
+++ mysite/catalog/urls.py
@@ -5,13 +5,6 @@ from views import *
 
 urlpatterns = [
 	url(r'^$', allcatalog),
-    #url(r'^posts/$', "<appname>.views.<function_name>"),
-    url(r'^addproduct/$', index, name='Index'),
-	url(r'^showdata/$', showdata),
-	url(r'^select/$', select),
-	url(r'^update/$', update),
-	url(r'^doUpdate/$', doUpdate),
-	url(r'^delete/$', delete),
 ]
 
 
